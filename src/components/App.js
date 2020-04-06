adeec811271cd7a500a767d0500acbbfdd2c8f5a
Parvez Shaikh <parvezshaikhj@gmail.com>
2020-04-07 00:00:14 +0530

    Showing components in the App

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { CommentBox } from './CommentBox'
+import { CommentList } from './CommentList'
 
 export const App = () => {
     return (
         <div>
             I'm the App component
+            <CommentBox />
+            <CommentList />
         </div>
     )
 }
---
+++ src/components/CommentBox.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const CommentBox = () => {
+    return (
+        <div>
+            I'm comment box
+        </div>
+    )
+}
