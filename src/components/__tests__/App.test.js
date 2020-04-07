9909a6fe5f66288e80fec2308fae128f0bbb0b65
Parvez Shaikh <parvezshaikhj@gmail.com>
2020-04-07 21:11:16 +0530

    Absolute Path Imports

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,17 +1,37 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 
-import { App } from '../App'
-import { CommentBox } from '../CommentBox'
-import { CommentList } from '../CommentList'
+import { App } from 'components/App'
+import { CommentBox } from 'components/CommentBox'
+import { CommentList } from 'components/CommentList'
 
+/***
+ * Test Structure
+ * it('description of the test', 'function containing our test logic')
+ * it: Global Function
+ * First Parameter: String description of the test we're running
+ * Second Parameter: Stuff to do when this tests run
+ */
 it('shows a comment box', () => {
+    // Create a new div element
     // const div = document.createElement('div');
 
+    // With the help of JSDOM, mount App into newly created div element. JSDOM is a library which interpret browser into command line.
     // ReactDOM.render(<App />, div);
 
+    // Looks inside the div and checks if the CommentBox is in there or not.
+    /**
+     * Test Structure
+     * expect("value that we're inspecting").match statement("value that we expect to see");
+     * expect: Global Function.
+     * value that we're inspecting: The thing we want to verify.
+     * match statement: Designates how we want to inspect the subject.
+     * value that we expect to see: Expected values, its what we want our 'subject' to be.
+     */
     // expect(div.innerHTML).toContain("I'm comment bo");
+    // Above expect statement is not the right way to detect the presence of the component i.e CommentBox.
 
+    // After everything is done, clean that div by removing App component
     // ReactDOM.unmountComponentAtNode(div);
 
     // Refactor using Enzyme:
@@ -23,27 +43,3 @@ it('shows a comment list', () => {
     const wrapped = shallow(<App />);
     expect(wrapped.find(CommentList).length).toEqual(1);
 });
-
-// Line: 7
-/***
- * Test Structure
- * it('description of the test', 'function containing our test logic')
- * it: Global Function
- * First Parameter: String description of the test we're running
- * Second Parameter: Stuff to do when this tests run
- */
-
-// Line: 8 -> Create a new div element
-// Line 10 -> With the help of JSDOM, mount App into newly created div element. JSDOM is a library which interpret browser into command line.
-// Line: 12 -> Looks inside the div and checks if the CommentBox is in there or not.
-/**
- * Test Structure
- * expect("value that we're inspecting").match statement("value that we expect to see");
- * expect: Global Function.
- * value that we're inspecting: The thing we want to verify.
- * match statement: Designates how we want to inspect the subject.
- * value that we expect to see: Expected values, its what we want our 'subject' to be.
- */
-
-// Line: 13 -> Above expect statement is not the right way to detect the presence of the component i.e CommentBox.
-// Line: 14 -> After everything is done, clean that div by removing App component
