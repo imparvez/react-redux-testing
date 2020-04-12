a2e2bb927f808f194e50363f93aae83ae27cf0d0
Parvez Shaikh <parvezshaikhj@gmail.com>
2020-04-12 23:04:49 +0530

    Describe statement in comment box test

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -21,38 +21,38 @@ it('shows a textarea and button', () => {
     // For failure: expect(wrapped.find('button').length).toEqual(12);
 });
 
-// Over here, this test is more of an interacting task.
-// Simulating Events
-// 1. Find the textarea elements
-// 2. Simulate a 'change' event [https://enzymejs.github.io/enzyme/docs/api/ReactWrapper/simulate.html]
-// 3. Provide a fake event object
-// 4. Force the component to update [https://enzymejs.github.io/enzyme/docs/api/ReactWrapper/update.html]
-// 5. Assert that the textarea value changed. [https://enzymejs.github.io/enzyme/docs/api/ReactWrapper/prop.html]
-it('has a textarea that users can type in', () => {
-    wrapped
-        .find('textarea') // 1
-        .simulate('change', { target: { value: 'Test comment'}}); // 2 & 3
-    // 4, immediately re-render the component after receiving the value of the textarea.
-    wrapped.update();
-    // 5. making sure that textarea is receiving the correct data/text/string
-    expect(wrapped.find('textarea').prop('value')).toEqual('Test comment');
-    // For failure: expect(wrapped.find('textarea').prop('value')).toEqual('sadjfhasjkd sadkjfhkst');
-});
+describe('the text area operation', () => {
+    // Over here, this test is more of an interacting task.
+    // Simulating Events
+    // 1. Find the textarea elements
+    // 2. Simulate a 'change' event [https://enzymejs.github.io/enzyme/docs/api/ReactWrapper/simulate.html]
+    // 3. Provide a fake event object
+    // 4. Force the component to update [https://enzymejs.github.io/enzyme/docs/api/ReactWrapper/update.html]
+    // 5. Assert that the textarea value changed. [https://enzymejs.github.io/enzyme/docs/api/ReactWrapper/prop.html]
+    beforeEach(() => {
+        wrapped
+            .find('textarea') // 1
+            .simulate('change', { target: { value: 'Test comment'}}); // 2 & 3
+        wrapped.update(); // 4: immediately re-render the component after receiving the value of the textarea.
+    })
 
-// When the input is submitted, textarea should be empty.
-it('should make textarea empty when input is submitted', () => {
-    wrapped
-        .find('textarea')
-        .simulate('change', { target: { value: 'Test comment'}});
-    wrapped.update();
+    it('has a textarea that users can type in', () => {
+        // 5. making sure that textarea is receiving the correct data/text/string
+        expect(wrapped.find('textarea').prop('value')).toEqual('Test comment');
+        // For failure: expect(wrapped.find('textarea').prop('value')).toEqual('sadjfhasjkd sadkjfhkst');
+    });
+    
+    // When the input is submitted, textarea should be empty.
+    it('should make textarea empty when input is submitted', () => {
+        wrapped
+            .find('form')
+            .simulate('submit');
+        wrapped.update();
+    
+        expect(wrapped.find('textarea').prop('value')).toEqual('');
+    });
+})
 
-    wrapped
-        .find('form')
-        .simulate('submit');
-    wrapped.update();
-
-    expect(wrapped.find('textarea').prop('value')).toEqual('');
-});
 
 // Component Clean Up
 afterEach(() => {
