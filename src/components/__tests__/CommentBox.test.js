aa13fa0134f553e9b4433d9c5a6d2c902785ec64
Parvez Shaikh <parvezshaikhj@gmail.com>
2020-04-12 22:53:09 +0530

    Simulating change events, providing fake objects on form textarea and button element

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -16,7 +16,42 @@ it('shows a textarea and button', () => {
     // console.log(wrapped.find('button').length);
 
     expect(wrapped.find('textarea').length).toEqual(1);
+    // For failure: expect(wrapped.find('textarea').length).toEqual(2);
     expect(wrapped.find('button').length).toEqual(1);
+    // For failure: expect(wrapped.find('button').length).toEqual(12);
+});
+
+// Over here, this test is more of an interacting task.
+// Simulating Events
+// 1. Find the textarea elements
+// 2. Simulate a 'change' event [https://enzymejs.github.io/enzyme/docs/api/ReactWrapper/simulate.html]
+// 3. Provide a fake event object
+// 4. Force the component to update [https://enzymejs.github.io/enzyme/docs/api/ReactWrapper/update.html]
+// 5. Assert that the textarea value changed. [https://enzymejs.github.io/enzyme/docs/api/ReactWrapper/prop.html]
+it('has a textarea that users can type in', () => {
+    wrapped
+        .find('textarea') // 1
+        .simulate('change', { target: { value: 'Test comment'}}); // 2 & 3
+    // 4, immediately re-render the component after receiving the value of the textarea.
+    wrapped.update();
+    // 5. making sure that textarea is receiving the correct data/text/string
+    expect(wrapped.find('textarea').prop('value')).toEqual('Test comment');
+    // For failure: expect(wrapped.find('textarea').prop('value')).toEqual('sadjfhasjkd sadkjfhkst');
+});
+
+// When the input is submitted, textarea should be empty.
+it('should make textarea empty when input is submitted', () => {
+    wrapped
+        .find('textarea')
+        .simulate('change', { target: { value: 'Test comment'}});
+    wrapped.update();
+
+    wrapped
+        .find('form')
+        .simulate('submit');
+    wrapped.update();
+
+    expect(wrapped.find('textarea').prop('value')).toEqual('');
 });
 
 // Component Clean Up
