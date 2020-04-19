d3fad959e74af56d80c87b8c4addef5047d1861e
Parvez Shaikh <parvezshaikhj@gmail.com>
2020-04-19 16:42:54 +0530

    Fetching a Remote Resource

--- src/Root.js
+++ src/Root.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import reducers from 'reducers/index';
+import reduxPromise from 'redux-promise'
 
-export default ({ children, initialState = {} }) => (
-    <Provider store={createStore(reducers, initialState, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())}>
-        {children}
-    </Provider>
-)
+export default ({ children, initialState = {} }) => {
+    const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+    const store = createStore(
+        reducers, 
+        initialState,
+        composeEnhancers(applyMiddleware(reduxPromise))
+    )
+    return (
+        <Provider store={store}>
+            {children}
+        </Provider>
+    )
+}
